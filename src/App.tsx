85f6a7d3c3f8beb49181edb0fd10951f621e67fc
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-05-18 00:45:12 +0700

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,84 @@
-import { Button } from "./components/button";
+import { ButtonSmall } from "./components/button";
 
 const tasks = [
   {
     id: 1,
     title: "Breakfast",
     completed: true,
-    date: new Date("2025-05-08 7:00"),
+    date: new Date("2025-05-20 8:00"),
   },
   {
     id: 2,
-    title: "Go to office",
+    title: "Go to work room",
     completed: false,
-    date: new Date("2025-05-08 9:00"),
+    date: new Date("2025-05-20 9:00"),
+  },
+  {
+    id: 3,
+    title: "Coding",
+    completed: false,
+    date: new Date("2025-05-20 10:00"),
+  },
+  {
+    id: 4,
+    title: "Lunch",
+    completed: false,
+    date: new Date("2025-05-20 12:00"),
+  },
+  {
+    id: 5,
+    title: "Meeting",
+    completed: false,
+    date: new Date("2025-05-20 13:00"),
+  },
+  {
+    id: 6,
+    title: "Working on projects",
+    completed: false,
+    date: new Date("2025-05-20 15:00"),
+  },
+  {
+    id: 7,
+    title: "Watch anime",
+    completed: false,
+    date: new Date("2025-05-20 17:00"),
+  },
+  {
+    id: 8,
+    title: "Dinner",
+    completed: false,
+    date: new Date("2025-05-20 19:00"),
+  },
+  {
+    id: 9,
+    title: "Sleep",
+    completed: false,
+    date: new Date("2025-05-20 22:00"),
   },
 ];
 
 export function App() {
   return (
-    <div>
-      <h1>Todo List</h1>
+    <div className="p-4 space-y-10">
+      <h1 className="text-3xl">Todo List</h1>
 
-      <ul>
+      <ul className="space-y-8">
         {tasks.map((task) => {
           return (
             <li key={task.id}>
-              <h2>{task.title}</h2>
-              <p>{task.completed ? "Completed" : "Incomplete"}</p>
-              <p>Date Time: {task.date.toLocaleString()}</p>
-              <Button>Delete</Button>
+              <h2 className="text-xl font-bold">{task.title}</h2>
+              <p className="font-bold">
+                {task.completed ? (
+                  <span className="text-green-500">🟢 Completed</span>
+                ) : (
+                  <span className="text-yellow-500">🟡 Incomplete</span>
+                )}
+              </p>
+              <p>
+                <span className="font-bold">Date Time: </span>
+                <span>{task.date.toLocaleString()}</span>
+              </p>
+              <ButtonSmall>Delete</ButtonSmall>
             </li>
           );
         })}
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,15 @@
 export function Button({ children }: { children: React.ReactNode }) {
-  return <button>{children}</button>;
+  return (
+    <button className="px-4 py-2 font-bold bg-emerald-300 text-emerald-800 rounded-lg text-sm">
+      {children}
+    </button>
+  );
+}
+
+export function ButtonSmall({ children }: { children: React.ReactNode }) {
+  return (
+    <button className="px-2 py-1 font-bold bg-red-300 text-red-800 rounded-lg text-xs">
+      {children}
+    </button>
+  );
 }
