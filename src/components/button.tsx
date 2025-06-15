a2b5b35442c65b0467d5716fc4ecb6a08d0f981d
Muhammad Rizki Kurnia Putra <muhammadrizki05@gmail.com>
2025-06-15 13:04:45 +0700

    installing react router

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,8 +6,6 @@ export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
     />
   );
 }
-//    <div className="p-4 bg-blue-400 flex justify-center">
-//      <button className="px-4 py-2 font-bold bg-blue-300 text-blue-800 rounded-lg text-sm">
 
 export function ButtonSmall(
   props: React.ButtonHTMLAttributes<HTMLButtonElement>
--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,7 +1,7 @@
 export function AboutRoute() {
   return (
-    <div className="p-10">
-      <h1 className="text-2xl">About Route</h1>
+    <div className="p-15">
+      <h1 className="text-5xl text-blue-400">This Is Page About </h1>
     </div>
   );
 }
